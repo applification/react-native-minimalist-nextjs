f9985f21fe60f461f6174668018835a4625794a9
Dave Hudson <dave@applificationhq.com>
2018-04-25 11:17:55 +0100

    README, updated example

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# React Native Minimalist with NextJS
+# React Native Minimalist in NextJS
+
+This is a sample repository to demonstrate using [React Native Minimalist](https://github.com/applification/react-native-minimalist) in Next JS.
+
+## Installation
+
+### Install dependencies
+
+```bash
+npm i
+```
+
+## Running in Development
+
+```bash
+npm run dev
+```
+
+## Running in Production
+
+### Build & Export Static Site
+
+```bash
+npm run build && npm run export
+```
+
+Builds a production version and save it in a folder named `out`.
+
+### Serve Static Site
+
+```bash
+npm i -g serve
+```
+
+```bash
+serve out
+```
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { HeaderText, Photo } from 'react-native-minimalist';
+import { StyleSheet, View } from 'react-native';
+import {
+  Content,
+  Header,
+  Photo,
+  Text,
+  Profile,
+  Divider,
+  PhotoInfo,
+  ListItem
+} from 'react-native-minimalist';
+import { IconCamera } from 'react-native-minimalist-icons';
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
+    flex: 1,
     alignItems: 'center',
-    height: '100%',
     justifyContent: 'center'
   },
-  text: {
-    alignItems: 'center',
-    fontSize: 24
+  mobile: {
+    width: 375
   }
 });
 
+const Camera = <IconCamera style={{ color: '#555', height: '1.6em' }} />;
+
 export default props => (
   <View style={styles.container}>
-    <Text style={styles.text}>Welcome to Next.js!</Text>
-    <HeaderText>With React Native Minimalist</HeaderText>
+    <View style={styles.mobile}>
+      <Header>Minimalist</Header>
+      <Text>john-tierney.com</Text>
+      <Divider fullWidth />
+      <PhotoInfo
+        photo={{
+          uri:
+            'https://scontent-lht6-1.cdninstagram.com/vp/8b749f16d5cadd6e680b479af5f237b0/5B68909F/t51.2885-19/10963846_377118132473154_1350059129_a.jpg'
+        }}
+        title="john_tierney_art"
+        icon={Camera}
+      />
+      <Photo
+        imageURL={
+          'https://scontent-lht6-1.cdninstagram.com/vp/43d6f9d19b29dbfc7b58c46a92ed425c/5B5770F5/t51.2885-15/e35/30602084_186676038631423_7930424840232632320_n.jpg'
+        }
+      />
+    </View>
   </View>
 );
